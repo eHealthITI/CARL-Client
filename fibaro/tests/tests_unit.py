5f97e16ea7a7a830103d467f3de0f0c4bb1e271c
Kimon Topouzidis <kimon.topouzidis@gmail.com>
2020-07-07 16:47:10 +0300

    added some basic adapter tests

--- fibaro/tests/tests_unit.py
+++ fibaro/tests/tests_unit.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from fibaro.validators import Event
+from fibaro.ypostirizo import Ypostirizo
 from fibaro.samples.events import SampleEvents
+from django.conf import settings
 import pydantic
 
 
@@ -15,4 +17,25 @@ class FibaroTestCase(TestCase):
         self.assertEqual(event.__class__, Event)
 
     def test_invalid_event(self):
-        self.assertRaises(pydantic.ValidationError, Event, **self.samples.invalid_event)
+        self.assertRaises(pydantic.ValidationError, Event,
+                          **self.samples.invalid_event)
+
+
+class FibaroAdapter(TestCase):
+    """Test the fibaro adapter (ypostirizo client -> ypostirizo cloud"""
+
+    def setUp(self):
+        self.samples = SampleEvents()
+
+    def test_valid_data(self):
+        """test a valid event data object sent to the cloud"""
+        response = Ypostirizo(self.samples.valid_event)._post()
+        self.assertEqual(response.status_code, 201)
+
+    def test_invalid_token(self):
+        """test a valid event data object sent to the cloud with wrong token"""
+        oldToken = settings.CLOUD_TOKEN
+        settings.CLOUD_TOKEN = 'A wrong token'
+        response = Ypostirizo(self.samples.valid_event)._post()
+        self.assertEqual(response.status_code, 401)
+        settings.CLOUD_TOKEN = oldToken
