3fd039db0f345a4cfe70c10b66f10b2c37ba42b3
Dimitrios Strantsalis <dstrants@gmail.com>
2020-07-08 16:05:31 +0300

    inserts device validator

--- fibaro/validators.py
+++ fibaro/validators.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from pydantic.types import List, OptionalInt
 
 from ypostirizoclient.settings import CLOUD_USER as user
 
@@ -23,3 +24,70 @@ class EventBase(BaseModel):
     deviceType: str
     event: Event
     user = user
+
+
+class DeviceProperties(BaseModel):
+    pollingTimeSec: int
+    wakeUpTime: int
+    zwaveCompany: str
+    zwaveInfo: str
+    zwaveVersion: str
+    alarmLevel: int
+    alarmType: int
+    batteryLevel: int
+    batterLowNotification: bool
+    categories: List[str]
+    configured: bool
+    dead: bool
+    deadReason: str
+    defInterval: int
+    deviceControlType: int
+    deviceIcon: int
+    emailNotificationID: int
+    emailNotificationType: int
+    endPointId: int
+    firmwareUpdate: dict
+    lastBreached: OptionalInt
+    log: str
+    logTemp: str
+    manufacturer: str
+    markasDead: bool
+    maxInterval: int
+    minInterval: int
+    model: str
+    nodeId: int
+    parametersTemplate: int
+    pendingActions: bool
+    productInfo: str
+    pushNotificationID: int
+    saveLogs: bool
+    serialNumber: str
+    smsNotificationID: int
+    smsmNotificationType: int
+    stepInterval: int
+    tamper: bool
+    updateVersion: str
+    useTemplate: bool
+    userDescription: str
+    value: bool
+
+
+class Device(BaseModel):
+    id: int
+    name: str
+    roomID: int
+    view: List[dict]
+    type: str
+    baseType: str
+    enabled: bool
+    visible: bool
+    isPlugin: bool
+    parentId: int
+    viewXml: bool
+    configXml: bool
+    interfaces: List[str]
+    properties: DeviceProperties
+    actions: dict
+    created: int
+    modified: int
+    sortOrder: int
