9b65fbd2f33e7c9b7ed2b65470975d5eb52adb36
Kimon Topouzidis <kimon.topouzidis@gmail.com>
2020-07-09 11:05:19 +0300

    move logic to ypostirizo

--- fibaro/ypostirizo.py
+++ fibaro/ypostirizo.py
@@ -1,5 +1,35 @@
 from .adapter import Cloud
 from .validators import EventBase
+import requests
+
+from django.conf import settings
+from .exceptions import InvalidToken, PageNotFound, CloudIsDown
+
+
+class Cloud():
+    """The class that describes functionality from ypostirizoClient
+    to ypostirizoCloud.
+    """
+
+    def __init__(self):
+        """Basic data initialization"""
+        self.token = settings.CLOUD_TOKEN
+        self.url = settings.CLOUD_URL
+
+    def send(self, endpoint='/device/events/', payload=None, method='GET'):
+        """Send data [event] to ypostirizoCloud"""
+        headers = {'authorization': f'Token {self.token}'}
+        response = requests.request(method, self.url+endpoint,
+                                    data=payload, headers=headers)
+        if not response.ok:
+            if response.status_code >= 500:
+                raise CloudIsDown
+            if response.status_code == 404:
+                raise PageNotFound
+            if response.json().get("detail") == 'Invalid token.':
+                raise InvalidToken
+            return response
+        return response
 
 
 class Ypostirizo():
