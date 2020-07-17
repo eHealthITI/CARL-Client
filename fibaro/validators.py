d169d6e57bd934c2447cbc19586c90c0153538e5
Kimon Topouzidis <kimon.topouzidis@gmail.com>
2020-07-17 11:48:36 +0300

    adjusted validators to fibaro hc lite

--- fibaro/validators.py
+++ fibaro/validators.py
@@ -6,41 +6,23 @@ from pydantic.types import List, Optional, OptionalInt
 from ypostirizoclient.settings import CLOUD_USER as user
 
 
-class EventData(BaseModel):
-    id: int
-    property: str
-    oldValue: float
-    newValue: float
-
-
-class Event(BaseModel):
-    type: str
-    data: EventData
-
-
 class EventBase(BaseModel):
     id: int
     type: str
     timestamp: int
     deviceID: int
     deviceType: str
-    event: Event
-    user = user
+    propertyName: str
+    oldValue = int
+    newValue = int
 
 
 class DeviceProperties(BaseModel):
-    UIMesageSendTime: OptionalInt
-    autoconfig: OptionalInt
-    alarmLevel: OptionalInt
-    alarmType: OptionalInt
-    batteryLevel: OptionalInt
-    batteryLowNotification: Optional[bool]
-    categories: Optional[List[str]]
+    UIMessageSendTime: OptionalInt
+    autoConfig: OptionalInt
     configured: bool
     date: Optional[str]
     dead: bool
-    deadReason: Optional[str]
-    defInterval: OptionalInt
     deviceControlType: int
     deviceIcon: int
     disabled: OptionalInt
@@ -48,16 +30,11 @@ class DeviceProperties(BaseModel):
     emailNotificationType: int
     endPoint: Optional[int]
     endPointId: int
-    firmwareUpdate: Optional[dict]
-    lastBreached: OptionalInt
-    liliOffCommand: Optional[str]
-    liliOnCommand: Optional[str]
+    homeIdHash: Optional[str]
     log: str
     logTemp: str
     manufacturer: str
     markAsDead: bool
-    maxInterval: OptionalInt
-    minInterval: OptionalInt
     model: str
     nodeId: int
     parameters: Optional[list]
@@ -65,26 +42,30 @@ class DeviceProperties(BaseModel):
     pendingActions: Optional[bool]
     pollingDeadDevice: Optional[bool]
     pollingTime: OptionalInt
-    pollingNextTime: OptionalInt
+    pollingTimeNext: OptionalInt
     pollingTimeSec: int
     productInfo: str
     pushNotificationID: int
     pushNotificationType: OptionalInt
     remoteGatewayId: OptionalInt
-    saveLogs: bool
+    requestNodeNeighborStatTimeStemp: int
+    requestNodeNeighborState: int
+    requestNodeNeighborStateTimeStemp: int
+    saveLogs: str
     serialNumber: str
+    showChildren: str
     smsNotificationID: int
     smsNotificationType: int
     status: Optional[str]
-    stepInterval: OptionalInt
-    tamper: Optional[bool]
-    updateVersion: Optional[str]
+    sunriseHour: str
+    sunsetHour: str
     useTemplate: bool
     userDescription: str
     value: Union[Optional[int], Optional[float], Optional[dict]]
-    wakeUpTime: OptionalInt
+    zwaveBuildVersion: str
     zwaveCompany: str
     zwaveInfo: str
+    zwaveRegion: str
     zwaveVersion: str
 
 
@@ -92,13 +73,13 @@ class Device(BaseModel):
     id: int
     name: str
     roomID: int
-    view: List[dict]
     type: str
     baseType: str
     enabled: bool
     visible: bool
     isPlugin: bool
     parentId: int
+    remoteGatewayId: int
     viewXml: bool
     configXml: bool
     interfaces: List[str]
