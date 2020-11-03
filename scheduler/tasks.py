04a84a65c5be6beb41b8372f4870b5e1e4de5ca4
Vasilis Alepopoulos <vasilisalepopoulos@gmail.com>
2020-11-03 12:24:42 +0200

    Refactoring

--- scheduler/tasks.py
+++ scheduler/tasks.py
@@ -17,20 +17,23 @@ app = Celery('tasks', broker='redis://localhost:6379//')
 
 @app.task
 def get_events_from_fibaro():
+    parameters = {}
     home_center_adapter = HomeCenterAdapter()
     event_max_time = fibaro.models.EventBase.objects.all().aggregate(Max('timestamp')).get('timestamp__max')
-    new_events = home_center_adapter.get(endpoint='/api/panels/event?from={}'.format(event_max_time)).json()
+    parameters['from'] = event_max_time
+
+    new_events = home_center_adapter.get(endpoint='/api/panels/event', parameters=parameters, method='GET').json()
     for new in new_events:
         event = fibaro.models.EventBase()
-        event.read_json(new)
-        event.save()
+        if new is not None:
+            event.read_json(new)
+            event.save()
 
 
 @app.task
 def get_events_from_local_db():
 
     events = fibaro.EventBase().objects.filter()
-
 
 
 @app.task
@@ -48,14 +51,14 @@ def find_devices():
 
 @app.task
 def send_to_cloud():
-    # this refers to type and not basetype
     # this task is going to be used to send the data to the cloud
-    current_time = int(datetime.datetime.now().timestamp())
-    latest_events = fibaro.EventBase().objects.filter(timestamp__gte=current_time-settings.DB_EVENT_INTERVAL)
+    current_time = int(datetime.now().timestamp())
+    latest_events = fibaro.models.EventBase.objects.filter(timestamp__gte=current_time-settings.DB_EVENT_INTERVAL)
     cloud = Cloud()
     event_json = serializers.serialize('json', latest_events)
-    cloud.push(event_json)
-
+    if event_json:
+        cloud.send(endpoint='/api/device/events/new_event/',
+                   payload=event_json)
 
 
 @app.task
