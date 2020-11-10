de43c92c1f769bcb50f2c3356fad5f7320b32374
Vasilis Alepopoulos <vasilisalepopoulos@gmail.com>
2020-11-10 12:20:06 +0200

    Added 2 more async tasks for Sections and Rooms

--- scheduler/celery.py
+++ scheduler/celery.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import, unicode_literals
 from celery import Celery
 import os
 
+from celery.schedules import crontab
+
 from ypostirizoclient import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ypostirizoclient.settings')
@@ -14,6 +16,14 @@ app = Celery('scheduler',
 app.config_from_object('scheduler.celeryconfig')
 
 app.conf.beat_schedule = {
+    'check-for-new-scenes': {
+        'task': 'scheduler.Fibaro.tasks.get_sections',
+        'schedule': crontab(hour=6)  # It will run everyday at 06:00 am
+    },
+    'check-for-new-rooms': {
+        'task': 'scheduler.Fibaro.tasks.get_rooms',
+        'schedule': crontab(hour=6)  # It will run everyday at 06:00 am
+    },
     'check-for-new-events': {
         'task': 'scheduler.Fibaro.tasks.get_events',
         'schedule': settings.HC_API_EVENT_INTERVAL
