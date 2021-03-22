31277c3d327b3d6ee0f2f0965a31b4c3621f3e60
Volpym <vasilisalepopoulos@gmail.com>
2021-03-22 19:35:00 +0200

    refactored get_ip.sh

--- get_ip.sh
+++ get_ip.sh
@@ -19,17 +19,17 @@ echo "found the IP of the router. ($router_ip)"
 fibaro_ip=$(nmap -sP $router_ip | awk '/^Nmap/{ip=$5}/Fibar/{print ip}')
 echo"found the ip of HC lite. ($fibaro_ip)"
 #Delete the last line the value to .env file
-sed -i '/^HC_URL/d' /home/pi/ypostirizoclient/.env
+sed -i '/^HC_URL/d' /home/pi/carlpi/.env
 
 #Appends HC_URL key and its value
-echo 'HC_URL='$fibaro_ip >> /home/pi/ypostirizoclient/.env
+echo 'HC_URL='$fibaro_ip >> /home/pi/carlpi/.env
 
 cronrab -r 
 crontab -l | { cat; echo "* 6 * * 5 update.sh"; } | crontab -
 
 echo "filled .env file"
 
-docker-compose -f /home/pi/ypostirizoclient/docker-compose.yml up --build
+docker-compose -f /home/pi/carlpi/docker-compose.yml up --build
 
 
 
