c8514e1cc21ecc77abd14faf2a2f547f9d93b524
Volpym <vasilisalepopoulos@gmail.com>
2021-03-24 10:44:38 +0200

    removed unused variables

--- ypostirizoclient/settings.py
+++ ypostirizoclient/settings.py
@@ -74,15 +74,11 @@ USE_L10N = True
 USE_TZ = True
 STATIC_URL = '/static/'
 # Project Specific params.
-# Change inside .env file
-SUBJECT_ID = cnf.SUBJECT_ID
 CLOUD_TOKEN = cnf.CLOUD_TOKEN
-#  HC_TOKEN = cnf.HC_TOKEN
 HC_PASSWORD = cnf.HC_PASSWORD
 HC_URL = cnf.HC_URL
 HC_USER = cnf.HC_USER
 CLOUD_URL = cnf.CLOUD_URL
-# CLOUD_USER = cnf.CLOUD_USER
 DEBUG = cnf.DEBUG
 IGNORED_DEVICES = ["HC_user","s", "com.fibaro.yrWeather", "com.fibaro.zwavePrimaryController",
                    "com.fibaro.niceEngine","master"]
